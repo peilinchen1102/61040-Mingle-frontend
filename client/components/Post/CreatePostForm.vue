8069c0aff5b79bab7bdc5a75e4de75917b58a4a9
Peilin <peili@mit.edu>
2023-10-29 22:55:49 -0400

    finish changing style

--- client/components/Post/CreatePostForm.vue
+++ client/components/Post/CreatePostForm.vue
@@ -26,7 +26,7 @@ const emptyForm = () => {
   <form @submit.prevent="createPost(content)">
     <label for="content">Post Contents:</label>
     <textarea id="content" v-model="content" placeholder="Create a post!" required> </textarea>
-    <button type="submit" class="pure-button-primary pure-button">Create Post</button>
+    <button type="submit" class="button-4">Create Post</button>
   </form>
 </template>
 
--- client/components/Post/EditPostForm.vue
+++ client/components/Post/EditPostForm.vue
@@ -24,8 +24,8 @@ const editPost = async (content: string) => {
     <textarea id="content" v-model="content" placeholder="Create a post!" required> </textarea>
     <div class="base">
       <menu>
-        <li><button class="btn-small pure-button-primary pure-button" type="submit">Save</button></li>
-        <li><button class="btn-small pure-button" @click="emit('editPost')">Cancel</button></li>
+        <li><button class="btn-small button-4 pure-button-primary" type="submit">Save</button></li>
+        <li><button class="btn-small button-4" @click="emit('editPost')">Cancel</button></li>
       </menu>
       <p v-if="props.post.dateCreated !== props.post.dateUpdated" class="timestamp">Edited on: {{ formatDate(props.post.dateUpdated) }}</p>
       <p v-else class="timestamp">Created on: {{ formatDate(props.post.dateCreated) }}</p>
